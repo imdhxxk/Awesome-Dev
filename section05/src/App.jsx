0c93614a596a39a35a84df45a47c8fb8f5a6a578
imdhxxk <barinlim77@gmail.com>
2026-06-12 06:20:39 +0900

    정보들을 각각  새로운 state를 이용해서 보관하는 방법

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,31 +1,11 @@
 import "./App.css"
-// import Header from "./components/Header"
-// import Main from "./components/Main"
-// import Footer from "./components/Footer"
-// import Button from "./components/Button"
-import Bulb from "./components/Bulb"
-import Counter from "./components/Counter"
+import Register from "./components/Register"
 
 function App() {
-  // const buttonProps = {
-  //   text : "메일",
-  //   color : "red",
-  //   a:1,
-  //   b:2,
-  //   c:3,
-  // }
+
   return (
     <>
-      {/* <Button {...buttonProps}/>
-      <Button text={"카페"}/>
-      <Button text={"블로그"}>
-        <Header/>
-      </Button> */}
-      {/* <Header/>
-      <Main/>
-      <Footer/>       */}
-      <Bulb/>
-      <Counter/>
+      <Register />
     </>
   )
 }
