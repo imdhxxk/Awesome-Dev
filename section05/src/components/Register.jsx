3b94fc977a1947f40c175bcd51c348ec831cfcfe
imdhxxk <barinlim77@gmail.com>
2026-06-12 12:23:12 +0900

    useRef를 사용해 렌더링후에 변화가 없어야되는 변수를 만듬

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -4,58 +4,75 @@
 // 국적
 // 자기소개
 
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Register = () => {
-    const [name, setName] = useState("이름")
-    const [birth, setBirth] = useState("생년월일")
-    const [country, setCountry] = useState("국적")
-    const [bio, setBio] = useState("")
+    const [input, setInput] = useState({
+        name : "",
+        birth:"",
+        country:"",
+        bio:""
+    })
 
-    const onChangeName = (e) => {
-        setName(e.target.value)
+    const countRef = useRef(0)
+    const inputRef = useRef()
+    console.log(input)
+    let count = 0;
+
+    const onChange = (e) => {
+        // countRef.current++;
+        count++; // 1만 나옴 이유는 리렌더링 될 때 count 변수도 같이 리렌더링 되면서 0으로 초기화가 되기 때문
+        console.log(countRef.current)
+        setInput({
+            ...input,
+            [e.target.name]:e.target.value
+        })
     }
 
-    const onChangeBirth = (e) => {
-        setBirth(e.target.value)
-    }
-
-    const onChangeCountry = (e) => {
-        setCountry(e.target.value)
-    }
-
-    const onChangeBio = (e) => {
-        setBio(e.target.value)
+    const onSubmit = () =>{
+        if(input.name ===""){
+            //이름을 입력하는 DOM요소 포커스
+            inputRef.current.focus()
+        }
     }
 
     return (
         <div>
+            
             <div>
                 <input
-                    value={name}
-                    onChange={onChangeName}
+                ref={inputRef}
+                    name="name"
+                    value={input.name}
+                    onChange={onChange}
                     placeholder={"이름"} />
             </div>
             <div>
                 <input 
-                    value={birth}
-                    onChange={onChangeBirth}
+                    name="birth"
+                    value={input.birth}
+                    onChange={onChange}
                     type="date"/>
             </div>
             <div>
                 <select 
-                    value={country} 
-                    onChange={onChangeCountry}>
+                    name="country"
+                    value={input.country} 
+                    onChange={onChange}>
                     <option value="kr" >한국</option>
                     <option value="us">미국</option>
                     <option value="uk" >영국</option>
                 </select>
             </div>
             <div>
-                <textarea value={bio} onChange={onChangeBio}>
+                <textarea 
+                    name="bio"
+                    value={input.bio} 
+                    onChange={onChange}>
                     
                 </textarea>
             </div>
+            <button onClick={onSubmit}>제출</button>
         </div>
     )
 }
