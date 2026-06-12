e415306b01e0e0b2b5a1a5e520da8e92e24303c7
imdhxxk <barinlim77@gmail.com>
2026-06-12 16:48:47 +0900

    TodoList 수정하기

--- section08/src/components/TodoItem.jsx
+++ section08/src/components/TodoItem.jsx
@@ -1,9 +1,14 @@
 import "./TodoItem.css"
 
-const TodoItem = ({id, isDone, content, date}) => {
+const TodoItem = ({id, isDone, content, date, onUpdate}) => {
+    
+    const onChangeCheckBox = () => {
+        onUpdate(id)
+    }
+
     return (
         <div className="TodoItem">
-            <input checked={isDone} type="checkbox" />
+            <input onChange={onChangeCheckBox} checked={isDone} type="checkbox" />
             <div className="content">{content}</div>
             <div className="date">{new Date(date).toLocaleDateString()}</div>
             <button>삭제</button>
