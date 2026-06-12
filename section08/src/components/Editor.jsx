668656582b9f5d18fdcb95c6d711df8231c885d7
imdhxxk <barinlim77@gmail.com>
2026-06-12 16:10:03 +0900

    TodoList todo추가하기 값이 들어있지 않을 때 포커스 잡기 추가 누르면 값 없어지기 엔터 누르면 버튼 누르기

--- section08/src/components/Editor.jsx
+++ section08/src/components/Editor.jsx
@@ -1,10 +1,40 @@
 import "./Editor.css"
+import { useState, useRef } from "react"
 
-const Editor = () => {
+const Editor = ({onCreate}) => {
+
+    const [content, setContent] = useState("")
+    const contentRef = useRef();
+
+    const onChangeContent = (e) => {
+        setContent(e.target.value)
+    }
+
+    const onKeydown = (e) => {
+        if(e.keyCode === 13){
+            onSubmit();
+        }
+    }
+
+    const onSubmit = ()=> {
+        if (content ===""){
+            contentRef.current.focus();
+            return
+        }
+        onCreate(content)
+        setContent("")
+    }
+
     return (
         <div className="Editor">
-            <input placeholder="새로운 투두"/>
-            <button>추가</button>
+            <input 
+                ref={contentRef}
+                value={content} 
+                onKeyDown={onKeydown}
+                onChange={onChangeContent} 
+                placeholder="새로운 todos.."
+            />
+            <button onClick={onSubmit}>추가</button>
         </div>
     )
 }
