c5ddac07f149c09cb5220d742b88a6205fd51627
imdhxxk <barinlim77@gmail.com>
2026-06-19 02:10:44 +0900

    useMemo를 사용해서 todo상태 출력

--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -1,6 +1,6 @@
 import "./List.css"
 import TodoItem from "./TodoItem"
-import {useState} from 'react'
+import {useState, useMemo} from 'react'
 
 const List = ({todos, onUpdate, onDelete}) => {
     const [search,setSearch] = useState("");
@@ -19,9 +19,33 @@ const List = ({todos, onUpdate, onDelete}) => {
 
     const filteredTodos = getFilteredData();
 
+
+
+    const {totalCount, doneCount, notDoneCount} =  useMemo(()=>{
+        console.log("getAnalyzedData호출")
+        const totalCount = todos.length
+        const doneCount = todos.filter(
+            (todo) => todo.isDone
+        ).length
+        const notDoneCount = totalCount - doneCount
+
+        return {
+            totalCount,
+            doneCount,
+            notDoneCount
+        }
+        
+    }, [todos])
+
+
     return (
         <div className="List">
             <h4>Todo List ♥️</h4>
+            <div>
+                <div>total: {totalCount}</div>
+                <div>done: {doneCount}</div>
+                <div>notDone; {notDoneCount}</div>
+            </div>
             <input 
                 value={search}
                 onChange={onChangeSearch}
