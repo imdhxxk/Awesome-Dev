48ef431d6e5581d5e13a04042d279719fde8e009
imdhxxk <barinlim77@gmail.com>
2026-06-26 07:35:35 +0900

    new 페이지 ui구현

--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,4 +1,15 @@
+import Header from "../components/Header"
+import Button from "../components/Button"
+import Editor from "../components/Editor"
+
+
 const New = () =>{
-    return <div></div>
+    return (
+        <div>
+            <Header title={"새일기 쓰기"}
+            leftChild={<Button text={"< 뒤로 가기"}/>}/>
+            <Editor/>
+        </div>
+    )
 }
 export default New
